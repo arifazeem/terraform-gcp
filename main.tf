aedf2ee5446da57503bd1d7702436646e4fa7fed
Azim <md.azim@rakuten.com>
2021-10-17 17:08:27 +0530

    added vpc

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-# terraform {
-#   backend "remote" {
-#     organization = "example-org-8fbf9e"
-#     workspaces {
-#       name = "terraform-gcp"
-#     }
-#   }
-# }
+terraform {
+  backend "remote" {
+    organization = "example-org-8fbf9e"
+    workspaces {
+      name = "terraform-gcp"
+    }
+  }
+}
 
-
-resource "google_storage_bucket" "static-sitei" {
-  name          = "image-test-store12"
-  location      = "EU"
-  force_destroy = true
-} 
+resource "google_compute_network" "vpc_network" {
+  project                 = var.project_id
+  name                    = "vpc-network"
+  auto_create_subnetworks = true
+  mtu                     = 1460
+}
