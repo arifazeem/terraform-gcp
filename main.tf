5fbf7038119d9238f1036689ee20baf1d738662d
Azim <md.azim@rakuten.com>
2021-10-17 10:34:14 +0530

    change the backend to gcs

--- main.tf
+++ main.tf
@@ -1,11 +1,18 @@
+# terraform {
+#   backend "remote" {
+#     organization = "example-org-8fbf9e"
+#     workspaces {
+#       name = "terraform-gcp"
+#     }
+#   }
+# }
 terraform {
-  backend "remote" {
-    organization = "example-org-8fbf9e"
-    workspaces {
-      name = "terraform-gcp"
-    }
+  backend "gcs" {
+    bucket = "terraform-gcs-mybucket"
+    prefix = "dev-filemage"
   }
 }
+
 resource "google_storage_bucket" "static-sitei" {
   name          = "image-test-store12"
   location      = "EU"
